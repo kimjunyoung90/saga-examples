844071d36f29a865de842dee456b44c00b266253
junyoungKim <wnsdud1900427@gmail.com>
2025-09-14 23:49:03 +0900

    chore: inventory service sql 수정

--- inventory/src/main/resources/data.sql
+++ inventory/src/main/resources/data.sql
@@ -1,5 +1,2 @@
-INSERT INTO inventory (product_id, quantity) VALUES (1, 50);
-INSERT INTO inventory (product_id, quantity) VALUES (2, 100);
-INSERT INTO inventory (product_id, quantity) VALUES (3, 0);
-INSERT INTO inventory (product_id, quantity) VALUES (4, 200);
-INSERT INTO inventory (product_id, quantity) VALUES (5, 10);
+INSERT INTO inventory (product_id, quantity) VALUES (1, 5);
+INSERT INTO inventory (product_id, quantity) VALUES (2, 0);
