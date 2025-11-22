69f201f2ccf32ca75e396517d2a87a46ef67f0de
junyoungKim <wnsdud1900427@gmail.com>
2025-11-23 00:28:36 +0900

    refactor: import 정리

--- orchestration/inventory/src/main/java/com/example/controller/InventoryController.java
+++ orchestration/inventory/src/main/java/com/example/controller/InventoryController.java
@@ -5,9 +5,10 @@ import com.example.entity.Inventory;
 import com.example.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/inventory")
