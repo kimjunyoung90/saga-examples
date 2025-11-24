0df47547361ba2a881a7da7243534825407a55b7
junyoungKim <wnsdud1900427@gmail.com>
2025-11-25 00:24:57 +0900

    feat: inventory service custom exception 추가

--- orchestration/inventory/src/main/java/com/example/service/InventoryService.java
+++ orchestration/inventory/src/main/java/com/example/service/InventoryService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.dto.InventoryRequest;
 import com.example.entity.Inventory;
+import com.example.exception.InventoryNotFoundException;
 import com.example.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,8 @@ public class InventoryService {
 
     @Transactional
     public Inventory create(InventoryRequest inventoryRequest) {
-        Inventory inventory = inventoryRepository.findByProductId(inventoryRequest.productId()).orElseThrow(() -> new IllegalArgumentException("상품 ID를 찾을 수 없습니다."));
+        Inventory inventory = inventoryRepository.findByProductId(inventoryRequest.productId())
+                .orElseThrow(() -> new InventoryNotFoundException("상품 ID를 찾을 수 없습니다."));
         inventory.deduct(inventoryRequest.quantity());
         return inventoryRepository.save(inventory);
     }
