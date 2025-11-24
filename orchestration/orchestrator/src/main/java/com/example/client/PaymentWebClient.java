64f46b3f55e829e433e8c125998a491e5f4c648c
김준영 <rlawnsdud05@douzone.com>
2025-11-24 11:56:54 +0900

    fix: saga-orchestration 결제 URI 수정

--- orchestration/orchestrator/src/main/java/com/example/client/PaymentWebClient.java
+++ orchestration/orchestrator/src/main/java/com/example/client/PaymentWebClient.java
@@ -17,7 +17,7 @@ public class PaymentWebClient {
     //결제 요청
     public Mono<PaymentResponse> createPayment(PaymentRequest paymentRequest) {
         return webClient.post()
-                .uri(baseUrl + "/payment")
+                .uri(baseUrl + "/payments")
                 .bodyValue(paymentRequest)
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, clientResponse -> Mono.empty())
@@ -26,7 +26,7 @@ public class PaymentWebClient {
 
     public Mono<Void> cancelPayment(Long paymentId) {
         return webClient.post()
-                .uri(baseUrl + "/payment/cancel/{paymentId}", paymentId)
+                .uri(baseUrl + "/payments/cancel/{paymentId}", paymentId)
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, clientResponse -> Mono.empty())
                 .bodyToMono(Void.class);
